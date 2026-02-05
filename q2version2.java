6a4d22a7e06a104fe5cdcd3b3e42aa6020ec5c17
mvobenedict <bkawortwe@st.ug.edu.gh>
2026-02-05 13:53:52 +0000

    q2v2

---
+++ q2version2.java
@@ -0,0 +1,8 @@
+public class ClassAverage {
+
+// Version 2: three scores
+    public static double calculateClassAverage(double s1, double s2, double s3) {
+        return (s1 + s2 + s3) / 3;
+    }
+
+}
